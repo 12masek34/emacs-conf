75ea40a27d4f5683a68de18fd53ee72408cfe7fe
dmitrijmartys <12masek34@mail.ru>
2023-01-02 21:36:52 +0300

    add

--- config.el
+++ config.el
@@ -382,6 +382,43 @@
   (activate-mark)
   )
 ;;=======================================================
+(defun get-point (symbol &optional arg)
+      "get the point"
+      (funcall symbol arg)
+      (point))
+
+    (defun copy-thing (begin-of-thing end-of-thing &optional arg)
+      "Copy thing between beg & end into kill ring."
+      (save-excursion
+        (let ((beg (get-point begin-of-thing 1))
+              (end (get-point end-of-thing arg)))
+          (copy-region-as-kill beg end))))
+
+(defun copy-word (&optional arg)
+      "Copy words at point into kill-ring"
+       (interactive "P")
+       (copy-thing 'backward-word 'forward-word arg)
+       ;;(paste-to-mark arg)
+     )
+
+
+(defun my-mark-word (N)
+  (interactive "p")
+  (if (and
+       (not (eq last-command this-command))
+       (not (eq last-command 'my-mark-word-backward)))
+      (set-mark (point)))
+  (forward-word N))
+
+
+(defun my-mark-word-backward (N)
+  (interactive "p")
+  (if (and
+       (not (eq last-command this-command))
+       (not (eq last-command 'my-mark-word)))
+      (set-mark (point)))
+  (backward-word N))
+;;=======================================================
 ;;limit
 (setq undo-limit 80000000)
 (setq scroll-margin 2)
@@ -558,6 +595,9 @@
     (global-set-key (kbd "s-b") 'magit-blame-addition)
     (global-set-key (kbd "s-M-q") 'beginning-of-defun)
     (global-set-key (kbd "s-M-e") 'end-of-defun)
+    (global-set-key (kbd "s-e") 'my-mark-word)
+    (global-set-key (kbd "s-q") 'my-mark-word-backward)
+    (global-set-key (kbd "s-C") 'copy-word)
 
     map))
 
