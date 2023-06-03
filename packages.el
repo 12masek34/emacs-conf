ac0ab68244dd8d1624a4164659030da47d9fa635
dmitrijmartys <12masek34@mail.ru>
2023-06-03 15:23:16 +0300

    add

--- packages.el
+++ packages.el
@@ -25,6 +25,7 @@
 (package! multi-vterm)
 (package! google-translate)
 (package! restclient)
+(package! telega)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
