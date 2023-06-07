68a6cd0c56893b5c3ceec9c4088d641dabe19a36
dmitrijmartys <12masek34@mail.ru>
2023-06-07 22:57:33 +0300

    add

--- packages.el
+++ packages.el
@@ -26,8 +26,7 @@
 (package! google-translate)
 (package! restclient)
 (package! alert)
-(package! telega
-  :recipe (:host github :repo "zevlg/telega.el" :branch "master"))
+(package! telega)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
