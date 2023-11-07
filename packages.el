5f91e72395f88d37a36970fd5292868c6fe8af11
Dmitriy_Martys <12masek34@mail.ru>
2023-11-07 22:25:10 +0300

    add

--- packages.el
+++ packages.el
@@ -20,6 +20,7 @@
 (package! restclient)
 (package! telega)
 (package! alert)
+(package! protobuf-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
