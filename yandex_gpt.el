05116cfc16147f323817ca3c8694d3db04c2a5a5
Dmitriy_Martys <12masek34@mail.ru>
2024-01-04 21:57:13 +0300

    add

--- yandex_gpt.el
+++ yandex_gpt.el
@@ -2,9 +2,15 @@
 (setq YANDEX_API_KEY (getenv "YANDEX_API_KEY"))
 (setq YANDEX_FOLDER_ID (getenv "YANDEX_FOLDER_ID"))
 (setq YANDEX_GPT_LOG_FILE (getenv "YANDEX_GPT_LOG_FILE"))
+(setq BALANCE_API_KEY (getenv "BALANCE_API_KEY"))
+(setq BILLING_ACCOUNT_ID (getenv "BILLING_ACCOUNT_ID"))
+(setq temperature "0.6")
+(setq maxTokens "2000")
 (setq separator_request ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 (setq separator_response "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 (setq request_id nil)
+(setq balance "unknow")
+
 
 (defun my/open-yandex-gpt-log ()
   (interactive)
@@ -41,6 +47,26 @@
   (setq text (buffer-substring (region-beginning) (region-end)))
   (my/requst-yandex-gpt-by-system (read-from-minibuffer "Yandex gpt: ")))
 
+(defun my/get-balance ()
+  (request "https://iam.api.cloud.yandex.net/iam/v1/tokens"
+        :type "POST"
+        :headers '(("Content-Type" . "application/json"))
+        :data (json-encode `(("yandexPassportOauthToken" . ,BALANCE_API_KEY)))
+        :parser 'json-read
+        :success (cl-function
+                (lambda (&key data &allow-other-keys)
+                  (setq iam_token (assoc-default 'iamToken data))
+                  (my/request-get-balance)))))
+
+(defun my/request-get-balance ()
+  (request (concat "https://billing.api.cloud.yandex.net/billing/v1/billingAccounts/" BILLING_ACCOUNT_ID)
+        :type "GET"
+        :headers `(("Authorization" . ,(concat "Bearer " iam_token)))
+        :parser 'json-read
+        :success (cl-function
+                (lambda (&key data &allow-other-keys)
+                  (setq balance (assoc-default 'balance data))))))
+
 (defun my/response-yandex-gpt ()
   (interactive)
   (sit-for 1)
@@ -57,16 +83,14 @@
 
 (defun my/requst-yandex-gpt ()
   (interactive)
+  (my/get-balance)
   (my/insert-text-and-open-temp-buffer (concat "\n" separator_request "\n" text) YANDEX_GPT_LOG_FILE)
   (request "https://llm.api.cloud.yandex.net/foundationModels/v1/completionAsync"
     :type "POST"
-    :data (json-encode `(
-                         ("modelUri" . ,(concat "gpt://" YANDEX_FOLDER_ID "/yandexgpt/latest"))
-                         ("completionOptions" . (
-                                                 ("stream" . nil)
-                                                 ("temperature" . "0.1")
-                                                 ("maxTokens" . "2000")
-                                                 ))
+    :data (json-encode `(("modelUri" . ,(concat "gpt://" YANDEX_FOLDER_ID "/yandexgpt/latest"))
+                         ("completionOptions" . (("stream" . nil)
+                                                 ("temperature" . ,temperature)
+                                                 ("maxTokens" . ,maxTokens)))
                          ("messages" . [(("role" . "user") ("text" . ,text))])))
     :headers `(("Content-Type" . "application/json")
                ("Authorization" . ,(concat "Api-Key " YANDEX_API_KEY))
@@ -75,25 +99,21 @@
     :success (cl-function
               (lambda (&key data &allow-other-keys)
                 (setq request_id (assoc-default 'id data))
-                (my/insert-text-and-open-temp-buffer (concat "\n\nrequest id => `" request_id "` send time => `" (format-time-string "%Y-%m-%d %H:%M:%S") "`\n" separator_request) YANDEX_GPT_LOG_FILE)
+                (my/insert-text-and-open-temp-buffer (concat "\n\nrequest id => `" request_id "` send time => `" (format-time-string "%Y-%m-%d %H:%M:%S") "` balance `" balance "`\n" separator_request) YANDEX_GPT_LOG_FILE)
                 (my/response-yandex-gpt)))))
 
 (defun my/requst-yandex-gpt-by-system (system)
   (interactive)
+  (my/get-balance)
   (my/insert-text-and-open-temp-buffer (concat "\n" separator_request "\n" "system => " system "\n\n" "text => " text "\n") YANDEX_GPT_LOG_FILE)
   (request "https://llm.api.cloud.yandex.net/foundationModels/v1/completionAsync"
     :type "POST"
-    :data (json-encode `(
-                         ("modelUri" . ,(concat "gpt://" YANDEX_FOLDER_ID "/yandexgpt/latest"))
-                         ("completionOptions" . (
-                                                 ("stream" . nil)
-                                                 ("temperature" . "0.1")
-                                                 ("maxTokens" . "2000")
-                                                 ))
-                         ("messages" . [
-                                        (("role" . "system") ("text" . ,system))
-                                        (("role" . "user") ("text" . ,text))
-                                        ])))
+    :data (json-encode `(("modelUri" . ,(concat "gpt://" YANDEX_FOLDER_ID "/yandexgpt/latest"))
+                         ("completionOptions" . (("stream" . nil)
+                                                 ("temperature" . ,temperature)
+                                                 ("maxTokens" . ,maxTokens)))
+                         ("messages" . [(("role" . "system") ("text" . ,system))
+                                        (("role" . "user") ("text" . ,text))])))
     :headers `(("Content-Type" . "application/json")
                ("Authorization" . ,(concat "Api-Key " YANDEX_API_KEY))
                ("x-folder-id" . YANDEX_FOLDER_ID))
@@ -101,5 +121,6 @@
     :success (cl-function
               (lambda (&key data &allow-other-keys)
                 (setq request_id (assoc-default 'id data))
-                (my/insert-text-and-open-temp-buffer (concat "\n\nrequest id => `" request_id "` send time => `" (format-time-string "%Y-%m-%d %H:%M:%S") "`\n" separator_request) YANDEX_GPT_LOG_FILE)
+                (my/insert-text-and-open-temp-buffer (concat "\n\nrequest id => `" request_id "` send time => `" (format-time-string "%Y-%m-%d %H:%M:%S") "` balance `" balance "`\n" separator_request) YANDEX_GPT_LOG_FILE)
                 (my/response-yandex-gpt)))))
+
