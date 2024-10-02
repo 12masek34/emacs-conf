ab997183f680b1af8f40a9136cf10e4458f9d529
Dmitriy Martys <martys.dmitriy@krasnoe-beloe.ru>
2024-10-02 17:06:51 +0300

    add

--- init.el
+++ init.el
@@ -102,7 +102,7 @@
        ;;(debugger +lsp)          ; FIXME stepping through code, to help you add bugs
        ;; debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       docker
+       ;; docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
@@ -203,7 +203,7 @@
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
+       swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        (web +lsp)               ; the tubes
        yaml              ; JSON, but readable
