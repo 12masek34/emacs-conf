81454ca144dfd056bb76154353fdedb86fa27c4a
Dmitriy Martys <martys.dmitriy@krasnoe-beloe.ru>
2025-10-17 08:36:04 +0300

    add

--- config.el
+++ config.el
@@ -207,7 +207,8 @@
   (setq telega-root-show-avatars t
         telega-user-show-avatars t
         telega-chat-show-avatars t
-        telega-root-auto-fill t)
+        telega-root-auto-fill t
+        telega-chat-show-deleted-messages-for 'all)
   (map! :map telega-msg-button-map "SPC" nil))
 
 ;; gpt
