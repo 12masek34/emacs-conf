91925321d2825812e0c555d0249e1e2e1658485d
Dmitriy Martys <martys.dmitriy@krasnoe-beloe.ru>
2026-02-10 08:35:49 +0300

    linux: Remove deprecated PostgreSQL database configurations.

--- database.el
+++ database.el
@@ -8,27 +8,3 @@
  :host "localhost"
  :database "postgres"
  :password "postgres")
-
-(sql-add-postgres-db
- monita_test
- :port 5432
- :user "martys"
- :host "192.168.0.142"
- :database "prod-kb-monita"
- :password "mTG54QhPg2IzSyzK")
-
-(sql-add-postgres-db
- monita_pricing
- :port 5432
- :user "postgres"
- :host "192.168.0.142"
- :database "pricing"
- :password "Itc4HSRe81oxwdbF")
-
-(sql-add-postgres-db
- game_server
- :port 5432
- :user "game_user"
- :host "localhost"
- :database "game"
- :password "game_pass")
